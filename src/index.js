f7d523ff43f644138353ae8222b798ae58fb7a1a
Roque Junior <roquejunior@ideiaseo.com>
2016-11-10 17:18:18 -0300

    JavaScript code base and first events

--- src/index.js
+++ src/index.js
@@ -7,32 +7,66 @@ import plugin from './lib/plugin';
 
 class Loader {
     constructor(element, options) {
-        const $element = $(element);
+        
+        this.$element = $(element);
 
-        // $(window).scroll(function () {
-        //     if ($(this).scrollTop() > options.offset) {
-        //         $element.fadeIn();
-        //     } else {
-        //         $element.fadeOut();
-        //     }
-        // });
-        //
-        // $element.click(function (e) {
-        //     e.preventDefault();
-        //
-        //     $('html, body').animate({
-        //         scrollTop: 0
-        //     }, options.speed);
-        // });
-        console.log( $element );
+        if(options.autoTrigger == true){
+
+            let $scrollElement = this.$element
+
+            switch(options.scrollContainer){
+                case 'window':
+                    $scrollElement = $(window)
+                    break;
+                case 'self':
+                default:
+                    $scrollElement = this.$element
+                    break;
+            }
+
+            options.onScroll = options.onScroll ? options.onScroll : this.containerOnScroll
+
+            $($scrollElement).on('scroll', options.onScroll)
+
+        }else{
+
+
+
+        }
+
     }
 
+    containerOnScroll(event){
+        
+        let $element = $(this)
+
+        console.log($(this).scrollTop() - $element.offset().top)
+        console.dir($element.data())
+
+        // if($(this).scrollTop() > options.offset){
+
+        // }
+
+    }
+
+    defaultDispatcher(){
+        console.log('In Dispatcher')
+    }
+
+    defaultDone(){
+        console.log('In Done')
+    }
 
 }
 
 Loader.DEFAULTS = {
     offset: 100,
     speed: 500,
+    autoTrigger: true,
+    onDispatch: Loader.defaultDispatcher,
+    onScroll: null,
+    onDone: Loader.defaultDone,
+    scrollContainer: 'self'
 };
 
 plugin('ideiaLoader', Loader);
