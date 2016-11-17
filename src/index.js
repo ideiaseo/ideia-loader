117210a3e12b257680f4d14e3f207f77c4c218cc
Roque Junior <roquejunior@ideiaseo.com>
2016-11-17 14:19:18 -0300

    Part 1 - Finish

--- src/index.js
+++ src/index.js
@@ -10,41 +10,50 @@ class Loader {
         
         let $element = $(element);
 
-        options.onScroll = options.onScroll ? options.onScroll : this.containerOnScroll
-        options.onDispatch = options.onDispatch ? options.onDispatch : this.defaultDispatcher
-        options.onDone = options.onDone ? options.onDone : this.defaultDone
+        options.onScroll = (options.onScroll == null) ? this.containerOnScroll : options.onScroll
+        options.onDispatch = (options.onDispatch == null) ? this.defaultDispatcher : options.onDispatch
+        options.onDone = (options.onDone == null) ? this.defaultDone : options.onDone
         
         $element.data('loader_options', options)
 
-        if(options.autoTrigger == true){
+        let $scrollElement = $element
 
-            let $scrollElement = $element
-
-            switch(options.scrollContainer){
-                case 'window':
-                    $scrollElement = $(window)
-                    break;
-                case 'self':
-                default:
-                    $scrollElement = $element
-                    break;
-            }
-
-            $($scrollElement).on('scroll', function doScrollPreEvent(event){
-                options.onScroll.call($element, event, $scrollElement)
-            })
-
-        }else{
-
+        switch(options.scrollContainer){
+            case 'window':
+                $scrollElement = $(window)
+                break;
+            case 'self':
+            default:
+                $scrollElement = $element
+                break;
         }
 
-    }
+        $element.data('scroller', $scrollElement)
 
-    containerOnScroll(event, scroller){
-        
+        if(options.autoTrigger == true){
+
+        	$($scrollElement).on('scroll', function doScrollPreEvent(event){
+
+		    	if(options.onScroll){
+		    		options.onScroll.call($element, event)
+		    	}
+        	})
+        }
+
+        $element.on('ideialoader.manual_dispatch', function manualDispatch(){
+        	let $element = $(this)
+        	let options = $element.data('loader_options')
+
+        	options.onDispatch.call(this)
+        })
+
+    }    
+
+    containerOnScroll(event){
+       
         let $element = $(this)
-        let $scroller = $(scroller)
         let options = $element.data('loader_options')
+        let $scroller = $element.data('scroller')
 
         let $scrollerPoint = $scroller.scrollTop() + $scroller.outerHeight()
         let $elementPoint = $element.offset().top + $element.outerHeight() - options.offset
@@ -58,52 +67,106 @@ class Loader {
     defaultDispatcher(){
         let $element = $(this)
         let options = $element.data('loader_options')
+        let $scroller = $element.data('scroller')
 
         if($element.data('loader_running')){
-            return;
+            return false;
         }
 
         $element.data('loader_running', true)
 
+        $element.trigger('ideialoader.pre_dispatch', [this])
+
         let method = $element.data('method')
         let url = $element.data('url')
+        let dataType = $element.data('dataType')
 
-        this.asyncLoad = $.ajax({
+        let ajaxOptions = options.ajaxOptions
+
+        if(method){
+        	ajaxOptions.method = method
+        }
+
+        if(url){
+        	ajaxOptions.url = url
+        }
+
+        $element.trigger('ideialoader.loading_start', [this])
+
+        this.asyncLoad = $.ajax(ajaxOptions).done(function preDone(data){
+
+        	if(options.onDone){
+        		options.onDone.call($element[0], data)
+        	}
             
-            'method': method ? method : 'GET',
-            'url': url ? url : window.location.href,
-            'dataType': 'html'
-
-        }).done(function preDone(data){
-
-            options.onDone.call($element[0], data)
-            $element.data('loader_running', false)
+            $element.trigger('ideialoader.post_dispatch', [this, data])
 
         }).fail(function(jqXHR, textStatus, errorThrown){
 
-            console.log(textStatus)
+            if(options.onFail){
+            	options.onFail.call($element[0], jqXHR, textStatus, errorThrown)
+            }
 
+            $element.trigger('ideialoader.fail_dispatch', [this, data])
+
+        }).always(function allwaysRun(){
+
+            $element.data('loader_running', false)
+            $element.trigger('ideialoader.loading_finish', [this])
+            
         })
-
     }
 
     defaultDone(data){
         let $element = $(this)
+        let options = $element.data('loader_options')
+
         let $doneContainer = $('<div></div>').addClass('ideia-loader-container')
 
         $doneContainer.append($(data))
-        $element.append($doneContainer);
+
+        switch(options.html.insertion){
+        	case 'prepend':
+        		$element.prepend($doneContainer)
+        		break
+
+        	case 'replace':
+        		$element.empty()
+        		$element.append($doneContainer)
+        		break
+
+        	case 'append':
+        	default:
+        		$element.append($doneContainer)
+        		break
+        }
+       
+    }
+
+    defaultFail(jqXHR, textStatus, errorThrown){
+
+        console.log(textStatus)
+
     }
 
 }
 
 Loader.DEFAULTS = {
     offset: 200,
+    html: {
+    	insertion: 'append', /*(append|prepend|replace) */
+    },
+    ajaxOptions: {
+    	dataType: 'html', /* html, json */
+    	method: 'GET',
+    	url: window.location.href,
+    },
     speed: 500,
     autoTrigger: true,
     onDispatch: null,
     onScroll: null,
     onDone: null,
+    onFail: null,
     scrollContainer: 'window'
 };
 
